e37e536716ba5df21a44ef63cd9666c3b30d3af2
Anna Karwacki <avk9238@rit.edu>
2018-05-26 09:19:20 +0900

    Game scene and game manager added

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+//important game info
+	//end game information
+	private int score = 0;
+	private int numMised = 0;
+	private int numHit = 0;
+	//scoring stuff
+	private int perfect = 10;
+	private int good = 5;
+	private int okay = 2;
+	private int wrong = -5;
+	private int miss = -10;
+
+	private bool pause
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
