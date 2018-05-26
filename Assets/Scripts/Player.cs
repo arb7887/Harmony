736c4116b6b84dd83e02ed8066923ed4909dd5bb
arb7887 <arb7887@g.rit.edu>
2018-05-25 21:36:11 -0400

    Player Script

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    //major = black orb
+    //minor = white orb
+    private Vector2 majorPos;
+    private Vector2 minorPos;
+
+    public GameObject major;
+    public GameObject minor;
+
+    public GameObject gameManager;
+
+    public float bottomLineY;
+    public float startX;
+
+	void Start () {
+        majorPos = major.transform.position;
+        minorPos = minor.transform.position;
+
+        majorPos.x = startX;
+        minorPos.x = -startX;
+	}
+	
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space)) Swap();
+	}
+
+    void Swap()
+    {
+
+    }
+}
