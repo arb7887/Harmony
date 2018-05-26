cc0fd23eafc3bfb16b3fa7c730b869311e02b8dc
Anna Karwacki <avk9238@rit.edu>
2018-05-26 11:45:07 +0900

    falling stuff

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -17,38 +17,40 @@ public class GameManager : MonoBehaviour {
 	private int wrong = -5;
 	private int miss = -10;
 
-<<<<<<< HEAD
 	private bool pause;
 	public AudioSource song;
 
 	public Text shownScore;
+	public GameObject majorP;
+	public GameObject miniorP;
 	public GameObject major;
 	public GameObject minior;
 
 	private float time;
-	private List<GameObject> particles;
-=======
-    private bool pause;
->>>>>>> d2a7755f723aef0b26f676b30d6e2a051899c29a
+	private List<GameObject> p;
+
+	private float fallTime;
+	private float fallDur;
 
 	// Use this for initialization
 	void Start () {
-		song.Play ();
-		particles = new List<GameObject> ();
+		//song.Play ();
+		p = new List<GameObject> ();
+		fallTime = 0.0f;
+		fallDur = .5f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		//check to make sure the song is still playing, when song is done game is done
-		shownScore = score.ToString();
+		//shownScore = score.ToString();
 		time += Time.deltaTime*1000;
-		if (time > 3000f && song.isPlaying) {
+		if (time > 3000f && time < 3100f) {
 			//buffer for spawning things
 			//check fo the current time and if its something that should happen, let it happen
 			//spawn particle at there spawn - time to travel to the bar
-			foreach(GameObject p in particles){
-				
-			}
+			SpawnParticle(1,0);
+			CheckPart ();
 		}
 		else {
 			//end game stuff
@@ -58,12 +60,31 @@ public class GameManager : MonoBehaviour {
 	void SpawnParticle(int color, int lane){
 		switch (lane) {
 		case 0:
-			if (color == 0)
-				particles.Add (Instantiate (major, new Vector3 (2, 4, 0), new Quaternion (0, 0, 0, 0)));
-			else (color == 1)
-				particles.Add(Instantiate(minior, new Vector3(-2,4,0), new Quaternion(0,0,0,0)));
+			if (color == 0) {
+				p.Add (Instantiate (majorP, new Vector3 (major.transform.position.x, 4, 0), new Quaternion (0, 0, 0, 0)));
+				p [p.Count - 1].GetComponent<Partical> ().Lane = 0;
+			} else if (color == 1) {
+				p.Add (Instantiate (miniorP, new Vector3 (minior.transform.position.x, 4, 0), new Quaternion (0, 0, 0, 0)));
+				p [p.Count - 1].GetComponent<Partical> ().Lane = 0;
+			}
+			break;
+		case 1:
+			if (color == 0) {
+				p.Add (Instantiate (majorP, new Vector3 (major.transform.position.x, 4, 0), new Quaternion (0, 0, 0, 0)));
+				p [p.Count - 1].GetComponent<Partical> ().Lane = 1;
+			} else if (color == 1) {
+				p.Add (Instantiate (miniorP, new Vector3 (major.transform.position.x, 4, 0), new Quaternion (0, 0, 0, 0)));
+				p [p.Count - 1].GetComponent<Partical> ().Lane = 1;
+			}
+			break;
 		}
 	}
-
+	void CheckPart(){
+		foreach (GameObject part in p) {
+			if (!part.GetComponent<Partical>().IsFalling) {
+				Destroy (part);
+			}
+		}
+	}
 	//get set
 }
